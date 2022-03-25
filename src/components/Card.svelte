c19e5b6c6e131492810a89abbc8543cc0756e840
Oguzdmr <doguzhan476@gmail.com>
2022-03-26 01:18:57 +0300

    first commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let word
+</script>
+
+
+<div class="card-word">
+    <h5>{word}</h5>
+</div>
+
+<style>
+    .card-word{
+        position: relative;
+        width: 100%;
+        height: 150px;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        background-color: purple;
+        color: white;
+        border-radius: 30px;
+        margin-top: 20px;
+    }
+    h5{
+        position: relative;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
